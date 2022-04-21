25edc8e99108b151cfea9df6bbf17f20dc02fdc8
David Ly <74430322+davidly1@users.noreply.github.com>
2022-04-21 02:21:10 -0400

    Update settings.java

--- app/src/main/java/com/example/lygridgame/settings.java
+++ app/src/main/java/com/example/lygridgame/settings.java
@@ -21,7 +21,6 @@ import java.io.IOException;
 
 //Name: David Ly
 //Date: January 11, 2021
-//Purpose: Unit 5 Project - Grid Game
 
 public class settings extends AppCompatActivity implements AdapterView.OnItemSelectedListener {
 
